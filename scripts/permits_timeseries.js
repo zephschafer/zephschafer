89644ecb4da00b679441ad3db02d6c18cf16bde8
Zeph <zephyr.schafer@gmail.com>
2022-08-03 17:26:16 -0700

    updates

--- scripts/permits_timeseries.js
+++ scripts/permits_timeseries.js
@@ -1,5 +1,5 @@
 import {Runtime, Inspector} from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js";
-import define from "https://api.observablehq.com/@zschafer/untitled.js?v=3";
+import define from "https://api.observablehq.com/@zschafer/pdx_permits_timeseries.js?v=3";
 new Runtime().module(define, name => {
   if (name === "display") return new Inspector(document.querySelector("#graph"));
   if (name === "viewof cells") return new Inspector(document.querySelector("#selector"));
