31a51c606ac6afff2c2aa79d4619eecc78b6d7f6
Zeph <zephyr.schafer@gmail.com>
2022-10-08 09:47:10 -0700

    Revert "Update permits_stacked_by_type.js"
    This reverts commit 5c52b845ab414393a079c365434bbd9cd4f98910.

--- scripts/permits_stacked_by_type.js
+++ scripts/permits_stacked_by_type.js
@@ -1,5 +1,5 @@
 import {Runtime, Inspector} from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js";
-import define from "https://api.observablehq.com/d/permits-by-type-public.js?v=3";
+import define from "https://api.observablehq.com/d/2a7c377606407e56.js?v=3";
 new Runtime().module(define, name => {
   if (name === "viewof selector") return new Inspector(document.querySelector("#permits-stacked-by-type-viewof-selector"));
 	if (name === "key") return new Inspector(document.querySelector("#permits-stacked-by-type-key"));
